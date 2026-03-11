e1266124b896bc093ef5dbfd46d1d70dc795c6e5
Rafael Zefanya Jaya Surya <rafze04@gmail.com>
2026-03-11 11:44:29 +0700

    third commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+import random
+
+
+def generate_strd_instance(seed, target_depth, D, p_contradict):
+
+    chain = []
+
+    for i in range(D):
+        pass
+
+    pass
+
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
